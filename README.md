64425ee30040ad7c7919642526c58f1b65712cdd
Chunyang Xu <xuchunyang56@gmail.com>
2015-03-19 23:54:31 +0800

    Add -name and -version options
    Allow user to explicitly set the name and the verion of port.

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # `port-create(1)`
 Generate MacPorts Portfile template
 
+## TODO
+- [x] Add [-name <name>] and [-version <version>] options
+- [ ] Generate tarball checksum, add [-no-fetch] option
+- [ ] Support Github-flavoured tarbarll URL, i.e.,
+      https://github.com/magit/magit/archive/1.2.2.tar.gz
+
 ## Note
 `portfile-gen.tcl` was copied from
 http://trac.macports.org/browser/contrib/portfile-gen/portfile-gen (2015-03-19)
--- port-create.tcl
+++ port-create.tcl
@@ -2,14 +2,7 @@
 #
 # Create a Portfile from a tarball URL
 #
-# Usage: ./port-create.tcl <URL>
-# example URL: https://www.kernel.org/pub/software/scm/git/git-2.3.3.tar.gz
-#
-# TODO:
-# - Add [--set-name <name] and [--set-version version] options
-# - Generate tarball checksum, add --no-fetch option
-# - Support Github-flavoured tarbarll URL, i.e.,
-#   https://github.com/magit/magit/archive/1.2.2.tar.gz
+# Example URL: https://www.kernel.org/pub/software/scm/git/git-2.3.3.tar.gz
 #
 
 # Get tarball filename from tarball URL
@@ -37,20 +30,32 @@ proc get_version {tarball} {
     }
 }
 
-if {[llength $::argv] != 1} {
-    puts stderr "Error: missing URL"
-    puts stderr "Usage: $::argv0 <URL>"
-    exit 1
+package require cmdline 1.3.1
+
+set options {
+    {url.arg      ""  "set the tarball URL"}
+    {name.arg     ""  "set the name of port"}
+    {version.arg  ""  "set the version of port"}
+}
+array set params [::cmdline::getoptions argv $options]
+
+set url $params(url)
+set name $params(name)
+set version $params(version)
+
+if {$url != ""} {
+    set tarball [get_tarball_filename $url]
+    if {$name == ""} {
+        set name [get_name $tarball]
+    }
+    if {$version == ""} {
+        set version [get_version $tarball]
+    }
 }
 
-set url [lindex $::argv 0]
-set tarball [get_tarball_filename $url]
-set portname [get_name $tarball]
-set portversion [get_version $tarball]
+puts "name:    $name"
+puts "version: $version"
 
-puts "name:    $portname"
-puts "version: $portversion"
-
-# exec ./portfile-gen.tcl $portname $portversion
+# exec ./portfile-gen.tcl $name $version
 
 # -*- coding: utf-8; mode: tcl; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- vim:fenc=utf-8:ft=tcl:et:sw=4:ts=4:sts=4
